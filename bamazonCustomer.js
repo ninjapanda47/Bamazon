08322a69a8ff44a43ac46428cc8d10aa96a375e3
Sandi <sandi.c.gau@gmail.com>
2017-03-04 14:26:35 -0600

    cleanup

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,47 +3,40 @@ var inquirer = require('inquirer');
 require("console.table");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "Bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "Bamazon"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  // console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+
 });
 
 var ask = function() {
-      inquirer.prompt([   
-            {           
-              type: "confirm",
-              name: "buy",
-              message: "Do you want to purchase an item?"
-              }
-              ]).then(function (answers) {
-                if (answers.buy === true){
-                  transaction();
-                }
-                else {
-                  console.log("Maybe next time!");
-                }
-      });
+    inquirer.prompt([{
+        type: "confirm",
+        name: "buy",
+        message: "Do you want to purchase an item?"
+    }]).then(function(answers) {
+        if (answers.buy === true) {
+            transaction();
+        } else {
+            console.log("Maybe next time!");
+        }
+    });
 }
 
 //function to show list of item
 var availableItems = function() {
-      connection.query('select * from products', function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      ask();
+    connection.query('select * from products', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        ask();
     });
-} 
+}
 
 availableItems();
 
@@ -51,44 +44,46 @@ availableItems();
 //function to run the transaction
 var transaction = function() {
 
-      inquirer.prompt([
-      	     {
-              name: "id",
-              message: "Enter the id of the item you would like to buy."
-            }, {
-              name: "quantity",
-              message: "Enter the quantity."
-            }, 
+    inquirer.prompt([{
+            name: "id",
+            message: "Enter the id of the item you would like to buy."
+        }, {
+            name: "quantity",
+            message: "Enter the quantity."
+        },
 
-      	]).then(function (answers) {
-           id = answers.id;
-           quantity = answers.quantity;
-           // console.log(id+ " " + quantity);
+    ]).then(function(answers) {
+        id = answers.id;
+        quantity = answers.quantity;
 
-        connection.query("select stock_quantity, price from products where ?", { item_id: id }, function(err, res) {
-        if (err) throw err;
+        connection.query("select stock_quantity, price from products where ?", {
+            item_id: id
+        }, function(err, res) {
+            if (err) throw err;
 
             var stock_quantity = res[0].stock_quantity;
             var price = res[0].price;
-            // console.log(stock_quantity + " " + price);
 
-          if (stock_quantity < quantity) {
-            console.log("Insufficient quantity!");
-          }
-            else {
-              newQuantity = stock_quantity - quantity; 
-              connection.query("update products set ? where ?", [{stock_quantity: newQuantity}, {item_id: id}], function(err, res) {
-               if (err) throw err;
-               showTotal(quantity, price);
-       
-              });
+            if (stock_quantity < quantity) {
+                console.log("Insufficient quantity!");
+            } else {
+                newQuantity = stock_quantity - quantity;
+                connection.query("update products set ? where ?", [{
+                    stock_quantity: newQuantity
+                }, {
+                    item_id: id
+                }], function(err, res) {
+                    if (err) throw err;
+                    showTotal(quantity, price);
+
+                });
             }
-          });
+        });
 
-      });
-}//end of transaction
+    });
+}
 
 var showTotal = function(quantity, price) {
-  var total = quantity * price;
-  console.log("your total purchase is " + total);
+    var total = quantity * price;
+    console.log("your total purchase is " + total);
 }
